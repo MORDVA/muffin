8d93a118647911419f0b7787cbde7eb79060fe5a
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-07-13 15:54:55 -0400

    Worked on the vm

--- main.c
+++ main.c
@@ -1,73 +1,101 @@
 #include <stdio.h>
 #include "globals.h"
 
+/* PROTOTYPES */
 
-/* fetch the next word from the program */
-int fetch()
+void loadProg(char* srcFilePath);
+
+/* GLOBALS */
+
+char* programLines[99];  /* Holds each line of the program */
+int jumpPoints[99];      /* Two parallel arrays*/
+char *jumpNames[99];
+int numberOfLines;
+
+/* The registers */
+
+typedef struct registers{
+
+} registerBank;
+
+registerBank REGISTERS;
+
+int main( int argc, char * argv[] )
 {
-  return program[ pc++ ];
-}
-
-/* decode a word */
-void decode( int instr )
-{
-  instrNum = (instr & 0xF000) >> 12;
-  reg1     = (instr & 0xF00 ) >>  8;
-  reg2     = (instr & 0xF0  ) >>  4;
-  reg3     = (instr & 0xF   );
-  imm      = (instr & 0xFF  );
-}
-
-/* the VM runs until this flag becomes 0 */
-int running = 1;
-
-/* evaluate the last decoded instruction */
-void eval()
-{
-  switch( instrNum )
-  {
-    case 0:
-      /* halt */
-      printf( "halt\n" );
-      running = 0;
-      break;
-    case 1:
-      /* loadi */
-      printf( "loadi r%d #%d\n", reg1, imm );
-      regs[ reg1 ] = imm;
-      break;
-    case 2:
-      /* add */
-      printf( "add r%d r%d r%d\n", reg1, reg2, reg3 );
-      regs[ reg1 ] = regs[ reg2 ] + regs[ reg3 ];
-      break;
-  }
-}
-
-/* display all registers as 4-digit hexadecimal words */
-void showRegs()
-{
-  int i;
-  printf( "regs = " );
-  for( i=0; i<NUM_REGS; i++ )
-    printf( "%04X ", regs[ i ] );
-  printf( "\n" );
-}
-
-void run()
-{
-  while( running )
-  {
-    showRegs();
-    int instr = fetch();
-    decode( instr );
-    eval();
-  }
-  showRegs();
-}
-
-int main( int argc, const char * argv[] )
-{
-  run();
+  loadProg(argv[1]);
   return 0;
 }
+
+void loadProg(char* srcFilePath){
+
+    printf("Executing %s\n", srcFilePath);
+
+    /* Loads program into the memory */
+
+    FILE *f = fopen(srcFilePath,"rb");
+
+    /* Loop Counter */
+    int i = 0;
+
+    /* The contents of the file */
+    char * buffer = 0;
+
+    /* The length of the file */
+    long length;
+
+    /* A pointer to each line of the file */
+    char* tokenPointer;
+
+    if (f)
+    {
+        fseek (f, 0, SEEK_END);
+        length = ftell (f);
+        fseek (f, 0, SEEK_SET);
+        buffer = malloc (length);
+        if (buffer)
+        {
+            fread (buffer, 1, length, f);
+        }
+        fclose (f);
+    }
+
+    if (buffer)
+    {
+        /* Data proccessing */
+
+        printf("\nRecieved File:\n%s\n",buffer);
+
+        /* Begin the proccess of splitting up all the strings into seperate commands */
+
+        tokenPointer = strtok(buffer,"\n");
+
+        while (tokenPointer != NULL)
+        {
+            programLines[i] = tokenPointer;
+            tokenPointer = strtok (NULL, "\n");
+            i++;
+        }
+
+        numberOfLines = i;
+
+    }
+
+}
+
+/** Parses line by line to determine every jump point
+*   Also sets instruction pointer to the start point
+*/
+
+void setPoints(){
+
+
+}
+
+/** Scans through the jump names until a match is found
+*   Then returns the parallel jumpPoints[] integer
+*/
+
+int getNewInstructionPointer(char* jumpName){
+
+}
+
